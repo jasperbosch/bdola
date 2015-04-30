f396ea80da51ccb14ebba85a5a3e42f5fe4aefa4
Jasper <jasperbosch@chello.nl>
2015-04-30 15:57:32 +0200

    d

--- server/models/checkin_db_config.php
+++ server/models/checkin_db_config.php
@@ -8,24 +8,24 @@ $dbVars = array (
 		'pass' => 'DDDhUMAEzx7Q'
 );
 
-try {
+// try {
 	$db = new PDO ( 'mysql:host=' . $dbVars ['host'] . ';dbname=' . $dbVars ['dbname'], $dbVars ['user'], $dbVars ['pass'] );
 	$db->setAttribute ( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 	$db->query ( "SET SESSION sql_mode = 'ANSI,ONLY_FULL_GROUP_BY'" );
-} catch ( PDOException $e ) {
-	$sMsg = '<p>
-            Regelnummer: ' . $e->getLine () . '<br />
-            Bestand: ' . $e->getFile () . '<br />
-            Foutmelding: ' . $e->getMessage () . '
-        </p>';
+// } catch ( PDOException $e ) {
+// 	$sMsg = '<p>
+//             Regelnummer: ' . $e->getLine () . '<br />
+//             Bestand: ' . $e->getFile () . '<br />
+//             Foutmelding: ' . $e->getMessage () . '
+//         </p>';
 	
-	$error = new error ();
-	$error->type = "danger";
-	$error->msg = $sMsg;
-	$errors [] = $error;
+// 	$error = new error ();
+// 	$error->type = "danger";
+// 	$error->msg = $sMsg;
+// 	$errors [] = $error;
 
-	trigger_error ( $sMsg );
-}
+// 	trigger_error ( $sMsg );
+// }
 
 
 define('SESSION_USER','userCakeUser');
